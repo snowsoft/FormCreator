2832e58fb265e0a45880cc073e266002fe060986
snowsoft <abdulkadirkar@gmail.com>
2016-12-02 00:51:20 +0300

    -

---
+++ src/Snowsoft/FormCreator/Elements/Checkbox.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Abdulkadir
+ * Date: 1.12.2016
+ * Time: 23:50
+ */
+
+namespace Snowsoft\FormCreator\Elements;
+
+
+class Checkbox
+{
+
+}
---
+++ src/Snowsoft/FormCreator/Elements/Input.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Abdulkadir
+ * Date: 1.12.2016
+ * Time: 23:49
+ */
+
+namespace Snowsoft\FormCreator\Elements;
+
+
+class Input
+{
+
+}
